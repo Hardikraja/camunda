108c52fc033fd484f39ac5a044a9fdbd9fcfe812
Marcos Barbero <marcos.barbero@camunda.com>
2026-03-15 20:34:10 +0100

    BACKPORT-CONFLICT

--- client-components/packages/camunda-api-zod-schemas/lib/8.8/role.ts
+++ client-components/packages/camunda-api-zod-schemas/lib/8.8/role.ts
@@ -104,7 +104,7 @@ const getRole: Endpoint<Pick<Role, 'roleId'>> = {
 	getUrl(params) {
 		const {roleId} = params;
 
-		return `/${API_VERSION}/roles/${roleId}`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}`;
 	},
 };
 
@@ -113,7 +113,7 @@ const updateRole: Endpoint<Pick<Role, 'roleId'>> = {
 	getUrl(params) {
 		const {roleId} = params;
 
-		return `/${API_VERSION}/roles/${roleId}`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}`;
 	},
 };
 
@@ -122,7 +122,7 @@ const deleteRole: Endpoint<Pick<Role, 'roleId'>> = {
 	getUrl(params) {
 		const {roleId} = params;
 
-		return `/${API_VERSION}/roles/${roleId}`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}`;
 	},
 };
 
@@ -138,7 +138,7 @@ const queryUsersByRole: Endpoint<Pick<Role, 'roleId'>> = {
 	getUrl(params) {
 		const {roleId} = params;
 
-		return `/${API_VERSION}/roles/${roleId}/users/search`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}/users/search`;
 	},
 };
 
@@ -147,7 +147,7 @@ const queryClientsByRole: Endpoint<Pick<Role, 'roleId'>> = {
 	getUrl(params) {
 		const {roleId} = params;
 
-		return `/${API_VERSION}/roles/${roleId}/clients/search`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}/clients/search`;
 	},
 };
 
@@ -156,7 +156,7 @@ const assignUserToRole: Endpoint<Pick<Role, 'roleId'> & {username: string}> = {
 	getUrl(params) {
 		const {roleId, username} = params;
 
-		return `/${API_VERSION}/roles/${roleId}/users/${username}`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}/users/${encodeURIComponent(username)}`;
 	},
 };
 
@@ -165,7 +165,7 @@ const unassignUserFromRole: Endpoint<Pick<Role, 'roleId'> & {username: string}> 
 	getUrl(params) {
 		const {roleId, username} = params;
 
-		return `/${API_VERSION}/roles/${roleId}/users/${username}`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}/users/${encodeURIComponent(username)}`;
 	},
 };
 
@@ -174,7 +174,7 @@ const assignClientToRole: Endpoint<Pick<Role, 'roleId'> & {clientId: string}> = 
 	getUrl(params) {
 		const {roleId, clientId} = params;
 
-		return `/${API_VERSION}/roles/${roleId}/clients/${clientId}`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}/clients/${encodeURIComponent(clientId)}`;
 	},
 };
 
@@ -183,7 +183,7 @@ const unassignClientFromRole: Endpoint<Pick<Role, 'roleId'> & {clientId: string}
 	getUrl(params) {
 		const {roleId, clientId} = params;
 
-		return `/${API_VERSION}/roles/${roleId}/clients/${clientId}`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}/clients/${encodeURIComponent(clientId)}`;
 	},
 };
 
@@ -192,7 +192,7 @@ const assignGroupToRole: Endpoint<Pick<Role, 'roleId'> & Pick<Group, 'groupId'>>
 	getUrl(params) {
 		const {roleId, groupId} = params;
 
-		return `/${API_VERSION}/roles/${roleId}/groups/${groupId}`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}/groups/${encodeURIComponent(groupId)}`;
 	},
 };
 
@@ -201,7 +201,7 @@ const unassignGroupFromRole: Endpoint<Pick<Role, 'roleId'> & Pick<Group, 'groupI
 	getUrl(params) {
 		const {roleId, groupId} = params;
 
-		return `/${API_VERSION}/roles/${roleId}/groups/${groupId}`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}/groups/${encodeURIComponent(groupId)}`;
 	},
 };
 
@@ -210,7 +210,7 @@ const queryGroupsByRole: Endpoint<Pick<Role, 'roleId'>> = {
 	getUrl(params) {
 		const {roleId} = params;
 
-		return `/${API_VERSION}/roles/${roleId}/groups/search`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}/groups/search`;
 	},
 };
 
@@ -219,7 +219,11 @@ const assignMappingToRole: Endpoint<Pick<Role, 'roleId'> & Pick<MappingRule, 'ma
 	getUrl(params) {
 		const {roleId, mappingId} = params;
 
+<<<<<<< HEAD
 		return `/${API_VERSION}/roles/${roleId}/mappings/${mappingId}`;
+=======
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}/mappings/${encodeURIComponent(mappingRuleId)}`;
+>>>>>>> 54734637 (fix: support forward slashes in entity IDs from OIDC providers (#45215))
 	},
 };
 
@@ -228,7 +232,11 @@ const unassignMappingFromRole: Endpoint<Pick<Role, 'roleId'> & Pick<MappingRule,
 	getUrl(params) {
 		const {roleId, mappingId} = params;
 
+<<<<<<< HEAD
 		return `/${API_VERSION}/roles/${roleId}/mappings/${mappingId}`;
+=======
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}/mappings/${encodeURIComponent(mappingRuleId)}`;
+>>>>>>> 54734637 (fix: support forward slashes in entity IDs from OIDC providers (#45215))
 	},
 };
 
@@ -237,7 +245,7 @@ const queryMappingRulesByRole: Endpoint<Pick<Role, 'roleId'>> = {
 	getUrl(params) {
 		const {roleId} = params;
 
-		return `/${API_VERSION}/roles/${roleId}/mapping-rules/search`;
+		return `/${API_VERSION}/roles/${encodeURIComponent(roleId)}/mapping-rules/search`;
 	},
 };
 
