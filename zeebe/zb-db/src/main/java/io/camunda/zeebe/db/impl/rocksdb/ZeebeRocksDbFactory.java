42078a5d0061c9f2230cab36bd54977c2968830c
rodrigolourencolopes <rodrigo.lopes@camunda.com>
2026-03-31 12:11:39 +0200

    feat: check that shared resources are initialized before closing.

--- zeebe/zb-db/src/main/java/io/camunda/zeebe/db/impl/rocksdb/ZeebeRocksDbFactory.java
+++ zeebe/zb-db/src/main/java/io/camunda/zeebe/db/impl/rocksdb/ZeebeRocksDbFactory.java
@@ -414,8 +414,12 @@ public final class ZeebeRocksDbFactory<
 
     @Override
     public void close() {
-      sharedWbm.close();
-      sharedCache.close();
+      if (isInitialized()) {
+        sharedWbm.close();
+        sharedCache.close();
+        sharedCache = null;
+        sharedWbm = null;
+      }
     }
   }
 
