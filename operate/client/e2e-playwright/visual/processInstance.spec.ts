d56fbe7ea5120557e2cd9ac76ef1f4e21748d22d
arienzIT <lucarienti@gmail.com>
2026-03-31 11:46:40 +0200

    refactor: implement auto-focus

--- operate/client/e2e-playwright/visual/processInstance.spec.ts
+++ operate/client/e2e-playwright/visual/processInstance.spec.ts
@@ -155,6 +155,7 @@ test.describe('process instance page', () => {
         name: /edit variable/i,
       })
       .click();
+    await processInstancePage.variablesEditor.waitForEditor();
 
     await expect(page).toHaveScreenshot();
   });
