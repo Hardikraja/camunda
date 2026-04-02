0f0cd7937009b16eb63666d44a3b4c12f7cdae3e
arienzIT <lucarienti@gmail.com>
2026-04-02 12:48:28 +0200

    refactor: address PR comments

--- operate/client/e2e-playwright/visual/processInstance.spec.ts
+++ operate/client/e2e-playwright/visual/processInstance.spec.ts
@@ -155,7 +155,7 @@ test.describe('process instance page', () => {
         name: /edit variable/i,
       })
       .click();
-    await processInstancePage.variablesEditor.waitForLoaded();
+    await processInstancePage.variablesEditor.waitForEditorToLoad();
 
     await expect(page).toHaveScreenshot();
   });
